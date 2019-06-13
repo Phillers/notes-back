b0c6b3cc44e9a7c22a329530e9f58aa5139c1a2c
Filip <phillers95@gmail.com>
2019-06-13 21:49:37 +0200

    tests

---
+++ integration/integration.js
@@ -0,0 +1,32 @@
+const expect = require('chai').expect;
+const request = require('supertest');
+
+/**
+ * Authenticate a cognito user and return its authentication token. Use the auth token in the authorization header
+ * @param callback Callback function with error as first param and the actual user token in the second param.
+ */
+
+
+describe('API — teamsByGame',() => {
+  const server = request("http://localhost:3000");
+  
+  it('POST /notes', (done) => {
+    server.post('/notes')
+      .send("{\"content\":\"hello world\",\"attachment\":\"hello.jpg\"}")
+      .expect(200)
+      .end((error, result) => {
+        if (error) return done(error);
+        console.log(result.body.noteId);
+        server.get('/notes/'+result.body.noteId)
+          .expect(200).end((error, result) => {
+            if (error) return done(error);
+            console.log(result.body);
+            expect(result.body).to.include({
+              attachment: 'hello.jpg',
+              content: 'hello world',
+            });
+            return done();
+          });
+      });
+  });
+});
